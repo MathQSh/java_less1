2c6fa3e1d31c548c2bcdab26628dafbe68e78693
MathQSh <miatiby@wp.pl>
2021-09-25 09:36:21 +0200

    https://github.com/kmuller86/java_lab_1.git

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Witaj świecie!");
+        System.out.println("To jest nasz pierwszy program w języku java");
+        System.out.println("Życzę" + " " + "wam " +  "miłej nauki");
 
     }
 }
